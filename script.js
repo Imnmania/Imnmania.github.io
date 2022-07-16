a1431165424a424b443c270c7cefd87d4ca0b31c
Imnmania <niloyb@live.com>
2022-07-16 20:13:04 +0600

    update on themes

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ for (var i = 0; themeDots.length > i; i++) {
 
 function setTheme(mode) {
     if (mode == 'light') {
-        document.getElementById('theme-style').href = 'default.css'
+        document.getElementById('theme-style').href = 'light.css'
     }
 
     if (mode == 'blue') {
